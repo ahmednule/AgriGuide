d84f238ed0436b24bb41639dbd9aaa6a04cebb22
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-20 19:14:34 +0300

    feat: Implement partnerships

---
+++ src/components/sections/Partnerships.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import SectionHeader from '../ui/SectionHeader'
+import Image from 'next/image';
+
+const Partnerships = () => {
+  return (
+    <section>
+      <SectionHeader>Partnerships</SectionHeader>
+      <div className="flex flex-col md:flex-row gap-8 md:gap-0 mx-10 justify-between items-center py-10 rounded-lg">
+        <div className="flex flex-col items-center justify-center w-full gap-2 md:gap-5">
+          <Image src="/assets/images/nasa.svg" alt="" width={75} height={75} />
+          <h3 className="text-lg text-emerald-700">NASA</h3>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full gap-2 md:gap-5">
+          <Image src="/assets/images/meta.svg" alt="" width={75} height={75} />
+          <h3 className="text-lg text-emerald-700">Meta</h3>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full gap-2 md:gap-5">
+          <Image
+            src="/assets/images/openai.svg"
+            alt=""
+            width={75}
+            height={75}
+          />
+          <h3 className="text-lg text-emerald-700">OpenAI</h3>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full gap-2 md:gap-5">
+          <Image
+            src="/assets/images/microsoft.svg"
+            alt=""
+            width={75}
+            height={75}
+          />
+          <h3 className="text-lg text-emerald-700">Microsoft</h3>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full gap-2 md:gap-5">
+          <Image
+            src="/assets/images/google.svg"
+            alt=""
+            width={75}
+            height={75}
+          />
+          <h3 className="text-lg text-emerald-500">Google</h3>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Partnerships
