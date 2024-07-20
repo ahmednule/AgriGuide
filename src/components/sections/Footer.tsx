32529cfa7d2f72221dab254843f0c286d6151d2c
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-20 19:12:34 +0300

    feat: Implement footer section

---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="p-5 mt-10 bg-emerald-900">
+      <p className="text-center text-emerald-50">&copy; AgriGuard 2024, All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
