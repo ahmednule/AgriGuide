5fa9b815ccb8df5a7ef90aa2dca17a120d2e6b17
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-20 19:15:01 +0300

    chore: Enable access to icons8 images site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
