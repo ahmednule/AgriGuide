d097ff72833f2c79e2e07fb7fa0b1baa608e4727
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-20 19:18:24 +0300

    feat: Add HowItWorks, Features, Testimonials, Partnerships, and FAQ sections to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Carousel from "@/components/sections/Carousel";
+import FAQ from "@/components/sections/FAQ";
+import Features from "@/components/sections/Features";
+import HowItWorks from "@/components/sections/HowItWorks";
+import Partnerships from "@/components/sections/Partnerships";
+import Testimonials from "@/components/sections/Testimonials";
 
 const Home = () => {
   return (
     <main>
       <Carousel />
+      <HowItWorks />
+      <Features />
+      <Testimonials />
+      <Partnerships />
+      <FAQ />
     </main>
   );
 };
