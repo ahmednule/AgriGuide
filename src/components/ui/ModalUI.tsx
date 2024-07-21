d6a70889f81a010337b3a7f075a83683a01dcb47
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-21 11:24:32 +0300

    feat: Add image scanning functionality

--- src/components/ui/ModalUI.tsx
+++ src/components/ui/ModalUI.tsx
@@ -11,14 +11,35 @@ import {
 import React from "react";
 import ImageUpload from "./ImageUpload";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faQrcode, faXmark } from "@fortawesome/free-solid-svg-icons";
+import SubmitButton from "./SubmitButton";
+import { useFormState } from "react-dom";
+import { scanImage } from "@/lib/actions";
+import { ScanStatus } from "@/lib/constants";
+import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
 
 const ModalUI = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [formState, formAction] = useFormState(scanImage, "");
+  const isScanSuccess =
+    formState === ScanStatus.SUCCESS
+      ? true
+      : formState === ScanStatus.ERROR
+      ? false
+      : undefined;
+
+  const { data, status } = useSession();
+  const user = data?.user;
+  // const isLoading = status === "loading";
+
+  const handleErrorToast = () => {
+    toast.error("You need to sign in to use this feature");
+  }
   return (
     <>
       <Button
-        onPress={onOpen}
+        onPress={user? onOpen : handleErrorToast}
         className="md:px-4 md:py-5 md:text-lg backdrop-blur-sm border bg-emerald-400/20 border-emerald-500/20 text-white rounded-full"
       >
         Start Diagnosis
@@ -27,8 +48,9 @@ const ModalUI = () => {
         isDismissable={false}
         size="3xl"
         isOpen={isOpen}
+        scrollBehavior="outside"
         classNames={{
-            backdrop: 'bg-emerald-950 bg-opacity-50',
+          backdrop: "bg-emerald-950 bg-opacity-50",
         }}
         onOpenChange={onOpenChange}
       >
@@ -43,26 +65,48 @@ const ModalUI = () => {
                   Upload an image of your plant or insect to start the diagnosis
                   process.
                 </p>
-                <ImageUpload name="image" />
-                <div className="flex flex-col items-center gap-5">
-                  <Button className="bg-emerald-700 text-white">Scan</Button>
+                <form action={formAction} className="flex flex-col">
+                  <ImageUpload name="image" />
+                  <SubmitButton
+                    endContent={<FontAwesomeIcon icon={faQrcode} />}
+                  >
+                    Scan
+                  </SubmitButton>
+                </form>
+                {formState && (
                   <Chip
                     endContent={
-                      <FontAwesomeIcon className="m-1" icon={faCheck} />
+                      <FontAwesomeIcon
+                        className="m-1"
+                        icon={isScanSuccess ? faCheck : faXmark}
+                      />
                     }
-                    color="success"
-                    className="text-white"
+                    color={isScanSuccess ? "success" : "danger"}
+                    className="text-white self-center"
                   >
-                    Scan successful
+                    {isScanSuccess ? "Success" : "Failed"}
                   </Chip>
-                </div>
-                <h3 className="text-lg font-bold text-emerald-950">Response</h3>
-                <p className="bg-emerald-200 p-5 rounded-xl">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Consequuntur voluptatem totam debitis. Amet impedit debitis
-                  expedita, et quos tenetur pariatur sequi magni ducimus,
-                  reiciendis sed eos vel in? Fugit, qui.
-                </p>
+                )}
+                {isScanSuccess && (
+                  <div>
+                    <h3 className="text-lg font-bold mb-4 text-emerald-950">
+                      Response
+                    </h3>
+                    <p className="bg-emerald-200 p-5 rounded-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Consequuntur voluptatem totam debitis. Amet impedit
+                      debitis expedita, et quos tenetur pariatur sequi magni
+                      ducimus, reiciendis sed eos vel in? Fugit, qui.
+                    </p>
+                    <br />
+                    <p className="bg-emerald-200 p-5 rounded-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Consequuntur voluptatem totam debitis. Amet impedit
+                      debitis expedita, et quos tenetur pariatur sequi magni
+                      ducimus, reiciendis sed eos vel in? Fugit, qui.
+                    </p>
+                  </div>
+                )}
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
