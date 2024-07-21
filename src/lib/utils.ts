3314b88e9bc59a84f5a3ca0d37210ba3e9c4c70e
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-21 11:23:31 +0300

    chore: Deduplicate auth function req

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,7 @@
-// Utility functions to perform tasks
+import { auth } from "@/auth";
+import { cache } from "react";
+
+export const cachedAuth = cache(auth);
+
+// Deduplicates request
+// Dont use it on server actions just server pages. We only wanna get cached version when we open our page
