16efa0077850efbfe41c4f24e67bd3df7b96dd4d
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-07-31 15:39:00 +0300

    refactor: Update Header component with admin functionality

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,7 +2,6 @@
 
 import { MENU } from "@/lib/constants";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Divider } from "@nextui-org/divider";
 import { Link } from "@nextui-org/link";
 import {
   Navbar,
@@ -13,20 +12,67 @@ import {
   NavbarMenuItem,
   NavbarMenuToggle,
 } from "@nextui-org/navbar";
-import { Avatar, Button, cn, Spinner, Tooltip } from "@nextui-org/react";
+import {
+  Avatar,
+  Button,
+  cn,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Spinner,
+  Tooltip,
+} from "@nextui-org/react";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 import AvatarDropdown from "./AvatarDropdown";
+import { Role } from "@prisma/client";
+import { makeAdmin, removeAdmin } from "@/lib/actions";
+import toast from "react-hot-toast";
 
 const Header = () => {
+  const [password, setPassword] = useState("");
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
   const isActive = (route: string) =>
     route === "/" ? pathname === "/" : pathname.startsWith(route);
 
-  const { data, status } = useSession();
+  const { data, status, update } = useSession();
   const user = data?.user;
   const isLoading = status === "loading";
+  const role = data?.user.role;
+  const isAdmin = role === Role.ADMIN;
+
+  const handleRemoveAdminClick = async () => {
+    try {
+      await removeAdmin(user!.id!);
+      toast.error("You are no longer an admin!");
+      update();
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    }
+  };
+
+  const handleAdminClick = async () => {
+    try {
+      await makeAdmin(user!.id!);
+      toast.success("You are now an admin!");
+      update();
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    }
+  };
+
+  const handleAdminSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password !== "Admin123") return toast.error("Invalid password!");
+    setPassword("");
+    await handleAdminClick();
+  };
 
   return (
     <Navbar
@@ -69,9 +115,48 @@ const Header = () => {
               Sign in
             </Button>
           )}
-          {isLoading && <Spinner color="success"/>}
-          {user && (
-            <AvatarDropdown email={user.email || ''} image={user.image || ''} name={user.name || ''}/>
+          {isLoading && <Spinner color="success" />}
+          {user && !isLoading && (
+            <AvatarDropdown
+              email={user.email || ""}
+              image={user.image || ""}
+              name={user.name || ""}
+            />
+          )}
+        </NavbarItem>
+        <NavbarItem>
+          {!isAdmin && (
+            <Popover placement="right">
+              <PopoverTrigger>
+                <Button
+                  isLoading={isLoading}
+                  className="!bg-emerald-700 text-white"
+                >
+                  Admin
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <form onSubmit={handleAdminSubmit} className="px-1 py-2">
+                  <Input
+                    isRequired
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter admin password"
+                  />
+                  <Button className="mt-2" isLoading={isLoading} type="submit">
+                    Submit
+                  </Button>
+                </form>
+              </PopoverContent>
+            </Popover>
+          )}
+          {isAdmin && (
+            <Button
+              isLoading={isLoading}
+              className="!bg-red-700 text-white"
+              onPress={handleRemoveAdminClick}
+            >
+              Remove Admin
+            </Button>
           )}
         </NavbarItem>
       </NavbarContent>
@@ -90,8 +175,6 @@ const Header = () => {
             </Link>
           </NavbarMenuItem>
         ))}
-        {/* <Divider className="my-2" /> */}
-       
       </NavbarMenu>
     </Navbar>
   );
