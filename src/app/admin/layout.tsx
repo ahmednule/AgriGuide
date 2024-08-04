fe42ac5df10a2aae30e0ebe162ace140e68f3b94
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-04 17:05:49 +0300

    feat: Add admin layout component

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,22 @@
+import { auth } from "@/auth";
+import AdminAside from "@/components/page/admin-page/AdminAside";
+import { Role } from "@prisma/client";
+import { notFound, useRouter } from "next/navigation";
+import React, { PropsWithChildren } from "react";
+
+const layout = async ({ children }: PropsWithChildren) => {
+  const session = await auth();
+  if (session?.user.role !== Role.ADMIN) {
+    notFound();
+  }
+  return (
+    <div className="h-[93vh] flex bg-emerald-50">
+      <AdminAside />
+      <main className="p-10 pt-20 lg:p-20 lg:w-5/6 h-full overflow-y-auto text-emerald-900">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default layout;
