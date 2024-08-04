1f2226ed8150dd54384778c3d6a541c5d12bb2e0
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-04 17:06:38 +0300

    feat: Create ConsultantsPage component for displaying consultant users

---
+++ src/app/admin/consultants/page.tsx
@@ -0,0 +1,31 @@
+import UserTable from "@/components/page/admin-page/UserTable";
+import prisma from "@/lib/prisma";
+import { Role } from "@prisma/client";
+import React from "react";
+
+const ConsultantsPage = async () => {
+  const consultants = await prisma.user.findMany({
+    where: {
+      role: Role.CONSULTANT,
+    },
+  });
+  const newConsultants = consultants.map((consultant) => {
+    return {
+      id: consultant.id,
+      image: consultant.image || null,
+      name: consultant.name as string,
+      email: consultant.email as string,
+      createdAt: consultant.createdAt,
+      updatedAt: consultant.updatedAt,
+      role: consultant.role,
+    };
+  });
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-emerald-900 mb-8">Consultants</h1>
+      <UserTable users={newConsultants} />
+    </>
+  );
+};
+
+export default ConsultantsPage;
