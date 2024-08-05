b6d6dde6e386a54dbe1fc5f0fd70a05c7d231173
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-05 09:05:29 +0300

    refactor: Update ImageUpload component to use forwardRef and add reset functionality

--- src/components/ui/ImageUpload.tsx
+++ src/components/ui/ImageUpload.tsx
@@ -1,19 +1,14 @@
-// Import React and FilePond
-import React from "react";
+// ImageUpload.tsx
+import React, { useRef, forwardRef, useImperativeHandle } from "react";
 import { FilePond, registerPlugin } from "react-filepond";
-
-// Import FilePond styles
 import "filepond/dist/filepond.min.css";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
-
-// Import FilePond plugins
 import FilePondPluginFileEncode from "filepond-plugin-file-encode";
 import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
 
-// Register the plugins
 registerPlugin(
   FilePondPluginImageExifOrientation,
   FilePondPluginImagePreview,
@@ -22,14 +17,25 @@ registerPlugin(
   FilePondPluginFileValidateSize
 );
 
-const ImageUpload = ({ name }: { name: string }) => {
+const ImageUpload = forwardRef(({ name }: { name: string }, ref?) => {
+  const pondRef = useRef<FilePond>(null);
+
+  useImperativeHandle(ref, () => ({
+    reset: () => {
+      if (pondRef.current) {
+        pondRef.current.removeFiles();
+      }
+    },
+  }));
+
   return (
     <div className="cursor-pointer">
       <FilePond
+        ref={pondRef}
         required
         allowReorder={false}
         acceptedFileTypes={["image/*"]}
-        name={name} // Set the field name for the file
+        name={name}
         labelFileTypeNotAllowed="Invalid file type. Please upload an image"
         allowMultiple={false}
         labelFileProcessingError="An error occurred during processing"
@@ -38,6 +44,9 @@ const ImageUpload = ({ name }: { name: string }) => {
       />
     </div>
   );
-};
+});
+
+// Add a display name to the component
+ImageUpload.displayName = "ImageUpload";
 
 export default ImageUpload;
