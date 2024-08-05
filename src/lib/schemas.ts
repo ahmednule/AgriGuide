13b7982f1f4dfe091f5ecbd0d29eea0a39284ec7
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-05 09:07:11 +0300

    chore: Add form validation schemas for adding pests and diseases

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -6,3 +6,22 @@ export const contactFormSchema = z.object({
   message: z.string().min(1, "Message is required"),
   db: z.string().optional(),
 });
+
+export const addPestFormSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().min(1, "Description is required"),
+  control: z.string().min(1, "Control is required"),
+  damage: z.string().min(1, "Damage is required"),
+  image: z.any(),
+  db: z.string().optional(),
+});
+
+export const addDiseaseFormSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  cause: z.string().min(1, "Cause is required"),
+  symptoms: z.string().min(1, "Symptoms is required"),
+  impact: z.string().min(1, "Impact is required"),
+  control: z.string().min(1, "Control is required"),
+  image: z.any(),
+  db: z.string().optional(),
+});
