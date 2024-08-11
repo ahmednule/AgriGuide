b6987fd1011cdcea51f2817461aa49f3baa6e903
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-11 18:10:14 +0300

    chore: Update next.config.mjs to include Supabase image storage configuration

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "img.icons8.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "cbrgfqvmkgowzerbzued.supabase.co",
+        pathname: "/storage/v1/object/public/images/**",
+      },
     ],
   },
 };
