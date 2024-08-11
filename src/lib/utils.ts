e3227997f6041b374e4da2b1a053825f7f4fa802
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-11 18:40:05 +0300

    feat: Add Showdown library for converting HTML to Markdown and vice versa

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,18 @@
 import { auth } from "@/auth";
 import { cache } from "react";
+import Showdown from "showdown";
 
 export const cachedAuth = cache(auth);
 
 // Deduplicates request
 // Dont use it on server actions just server pages. We only wanna get cached version when we open our page
+
+const converter = new Showdown.Converter();
+
+export const convertHtmlToMarkdown = (html: string): string => {
+  return converter.makeMarkdown(html);
+};
+
+export const convertMarkdownToHtml = (markdown: string): string => {
+  return converter.makeHtml(markdown);
+};
