ef17fe2230994f5ae6015c39df2ce7ac2aacb282
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-11 18:39:08 +0300

    refactor: Update SubmitButton component to accept color and onPress props

--- src/components/ui/SubmitButton.tsx
+++ src/components/ui/SubmitButton.tsx
@@ -1,21 +1,36 @@
-'use client'
+"use client";
 
 import { Button, cn } from "@nextui-org/react";
 import React from "react";
 import { useFormStatus } from "react-dom";
 
-const SubmitButton = ({children, endContent}: {
+const SubmitButton = ({
+  children,
+  endContent,
+  color,
+  onPress,
+}: {
   children: React.ReactNode;
   endContent?: React.ReactNode;
+  color?:
+    | "default"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "warning"
+    | "danger";
+  onPress?: () => void;
 }) => {
   const { pending } = useFormStatus();
   return (
     <Button
+      onPress={onPress}
       type="submit"
       isLoading={pending}
+      color={color}
       size="lg"
       className={cn(
-        "bg-emerald-400 self-center font-bold shadow-[0_4px_8px_rgb(0_0_0/0.2)]"
+        {"bg-emerald-400 self-center font-bold shadow-[0_4px_8px_rgb(0_0_0/0.2)]": !color}
       )}
       endContent={endContent}
     >
