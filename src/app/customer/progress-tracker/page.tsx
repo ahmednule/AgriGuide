e979680d042c4a9eae0742007394e451fb7dd171
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-18 19:20:50 +0300

    feat: Update ProgressTracker page to display disease images and tags

--- src/app/customer/progress-tracker/page.tsx
+++ src/app/customer/progress-tracker/page.tsx
@@ -15,16 +15,32 @@ const ProgressTracker = async () => {
   const diseases = await prisma.scan.findMany({
     where: {
       customerId: user.id,
-      type: ScanType.DISEASE
+      type: ScanType.DISEASE,
     },
+  });
+
+  const tags = await prisma.scan.findMany({
+    where: {
+      customerId: user.id,
+      type: ScanType.DISEASE,
+    },
+    select: {
+      tag: true,
+    },
+    distinct: ["tag"],
   });
 
   return (
     <div>
       <section>
         <h1 className="text-3xl font-bold">Progress Tracker</h1>
-        <p className="mt-6">Select 2 images to scan the progress overtime.</p>
-        <DiseaseList diseases = {diseases} />
+        {diseases.length > 0 ? (
+          <>
+            <DiseaseList tags={tags} diseases={diseases} />
+          </>
+        ) : (
+          <p className="mt-6">No disease images found.</p>
+        )}
       </section>
     </div>
   );
