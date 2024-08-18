1f306f96655cb0751678ea5dab203cee2b25432f
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-18 19:23:59 +0300

    refactor: Update SectionHeader component to accept custom className prop

--- src/components/ui/SectionHeader.tsx
+++ src/components/ui/SectionHeader.tsx
@@ -1,7 +1,11 @@
+import { cn } from "@nextui-org/react";
 import React, { PropsWithChildren } from "react";
 
-const SectionHeader = ({ children }: PropsWithChildren) => {
-  return <h2 className=" text-center font-bold text-3xl text-emerald-950 my-10">{children}</h2>;
+const SectionHeader = ({ children, className }: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return <h2 className={cn(" text-center font-bold text-3xl text-emerald-950 my-10", className)}>{children}</h2>;
 };
 
 export default SectionHeader;
