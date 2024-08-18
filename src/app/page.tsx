0f37c300e5f9766b837dea74b40f8e4de41c6ce1
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-18 19:19:56 +0300

    refactor: Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,19 @@
 import Carousel from "@/components/page/home-page/Carousel";
-import Contact from "@/components/page/home-page/Contact";
 import FAQ from "@/components/page/home-page/FAQ";
 import Features from "@/components/page/home-page/Features";
-import HowItWorks from "@/components/page/home-page/HowItWorks";
 import Partnerships from "@/components/page/home-page/Partnerships";
+import Process from "@/components/page/home-page/Process";
 import Testimonials from "@/components/page/home-page/Testimonials";
-
 
 const Home = () => {
   return (
     <main>
       <Carousel />
-      <HowItWorks />
+      <Process />
       <Features />
       <Testimonials />
       <Partnerships />
       <FAQ />
-      <Contact />
     </main>
   );
 };
