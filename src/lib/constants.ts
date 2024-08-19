883d26b89206895a633c893c011d80d3a16438f9
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-19 20:34:43 +0300

    refactor: Shift testimonials data to data file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,4 @@
 import {
-  faBug,
   faClockRotateLeft,
   faCog,
   faHouse,
@@ -8,7 +7,6 @@ import {
   faUser,
   faUserDoctor,
   faUserTie,
-  faVirus,
 } from "@fortawesome/free-solid-svg-icons";
 
 export const initialFormState = {
@@ -66,33 +64,6 @@ export const MENU = [
   {
     route: "#contact",
     value: "Contact",
-  },
-];
-
-export const TESTIMONIALS = [
-  {
-    quote:
-      "As a smallholder farmer, the AI-powered app has been a game-changer for me. Identifying pests and diseases used to be guesswork, but now I get accurate diagnoses and treatment recommendations within minutes. My crop yields have improved significantly, and I feel more confident in managing my farm.",
-    name: "John Gikuru",
-    title: "Kenyan Farmer",
-  },
-  {
-    quote:
-      "This app has revolutionized our agricultural extension services. We can now provide timely and precise advice to farmers, even in remote areas. The AI's accuracy and the app's user-friendly interface have made it an invaluable tool for both farmers and extension officers.",
-    name: "Amina Swaleh",
-    title: "Agricultural Extension Officer",
-  },
-  {
-    quote:
-      "Using the app has saved me so much time and money. The targeted treatment recommendations mean I use pesticides more efficiently, and the emphasis on organic options helps me maintain sustainable farming practices. It's a must-have for any farmer looking to improve their crop management.",
-    name: "Samuel Ndokii",
-    title: "Nigerian Farmer",
-  },
-  {
-    quote:
-      "Our agribusiness has benefited greatly from this AI app. It ensures our field staff can consistently identify and manage crop threats, reducing reliance on broad-spectrum pesticides and promoting sustainable practices. The app's data insights have also been valuable for our research and development.",
-    name: "Grace Wairimu",
-    title: "Agribusiness Manager",
   },
 ];
 
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -7,7 +7,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { TFeatureItem } from "./types";
 
-export const FEATURES: TFeatureItem[] = [
+export const FEATURES_DATA: TFeatureItem[] = [
   {
     title: "Instant Diagnosis",
     description:
@@ -47,7 +47,7 @@ export const FEATURES: TFeatureItem[] = [
   },
 ];
 
-export const PARTNERSHIPS = [
+export const PARTNERSHIPS_DATA = [
   { src: "/assets/images/nasa.svg", alt: "NASA logo", name: "NASA" },
   { src: "/assets/images/meta.svg", alt: "Meta logo", name: "Meta" },
   { src: "/assets/images/openai.svg", alt: "OpenAI logo", name: "OpenAI" },
@@ -89,5 +89,32 @@ export const FAQ_DATA = [
     content: `Yes, the app emphasizes sustainable practices by recommending organic
               control methods whenever possible and providing targeted pesticide use
               only when necessary.`,
+  },
+];
+
+export const TESTIMONIALS_DATA = [
+  {
+    quote:
+      "As a smallholder farmer, the AI-powered app has been a game-changer for me. Identifying pests and diseases used to be guesswork, but now I get accurate diagnoses and treatment recommendations within minutes. My crop yields have improved significantly, and I feel more confident in managing my farm.",
+    name: "John Gikuru",
+    title: "Kenyan Farmer",
+  },
+  {
+    quote:
+      "This app has revolutionized our agricultural extension services. We can now provide timely and precise advice to farmers, even in remote areas. The AI's accuracy and the app's user-friendly interface have made it an invaluable tool for both farmers and extension officers.",
+    name: "Amina Swaleh",
+    title: "Agricultural Extension Officer",
+  },
+  {
+    quote:
+      "Using the app has saved me so much time and money. The targeted treatment recommendations mean I use pesticides more efficiently, and the emphasis on organic options helps me maintain sustainable farming practices. It's a must-have for any farmer looking to improve their crop management.",
+    name: "Samuel Ndokii",
+    title: "Nigerian Farmer",
+  },
+  {
+    quote:
+      "Our agribusiness has benefited greatly from this AI app. It ensures our field staff can consistently identify and manage crop threats, reducing reliance on broad-spectrum pesticides and promoting sustainable practices. The app's data insights have also been valuable for our research and development.",
+    name: "Grace Wairimu",
+    title: "Agribusiness Manager",
   },
 ];
