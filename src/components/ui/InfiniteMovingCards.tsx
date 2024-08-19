165fefaccea2956e61a808c2524d8f1fe20018e3
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-19 20:05:18 +0300

    refactor: Improve performance and semanticness of InfiniteMovingCards component

--- src/components/ui/InfiniteMovingCards.tsx
+++ src/components/ui/InfiniteMovingCards.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Avatar, cn } from "@nextui-org/react";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 export const InfiniteMovingCards = ({
   items,
@@ -23,11 +23,37 @@ export const InfiniteMovingCards = ({
   const containerRef = React.useRef<HTMLDivElement>(null);
   const scrollerRef = React.useRef<HTMLUListElement>(null);
 
-  useEffect(() => {
-    addAnimation();
-  }, []);
   const [start, setStart] = useState(false);
-  function addAnimation() {
+
+  const getDirection = useCallback(() => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  }, [containerRef, direction]);
+
+  const getSpeed = useCallback(() => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "80s");
+      }
+    }
+  }, [containerRef, speed]);
+
+  const addAnimation = useCallback(() => {
     if (containerRef.current && scrollerRef.current) {
       const scrollerContent = Array.from(scrollerRef.current.children);
 
@@ -42,33 +68,12 @@ export const InfiniteMovingCards = ({
       getSpeed();
       setStart(true);
     }
-  }
-  const getDirection = () => {
-    if (containerRef.current) {
-      if (direction === "left") {
-        containerRef.current.style.setProperty(
-          "--animation-direction",
-          "forwards"
-        );
-      } else {
-        containerRef.current.style.setProperty(
-          "--animation-direction",
-          "reverse"
-        );
-      }
-    }
-  };
-  const getSpeed = () => {
-    if (containerRef.current) {
-      if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "20s");
-      } else if (speed === "normal") {
-        containerRef.current.style.setProperty("--animation-duration", "40s");
-      } else {
-        containerRef.current.style.setProperty("--animation-duration", "80s");
-      }
-    }
-  };
+  }, [getDirection, getSpeed]);
+
+  useEffect(() => {
+    addAnimation();
+  }, [addAnimation]);
+
   return (
     <div
       ref={containerRef}
@@ -93,26 +98,22 @@ export const InfiniteMovingCards = ({
             }}
             key={item.name}
           >
-            <blockquote className="flex flex-col h-full ">
+            <div className="flex flex-col h-full ">
               <div
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 text-sm leading-[1.6] text-slate-200 font-normal">
+              <blockquote className=" relative z-20 text-slate-200">
                 {item.quote}
-              </span>
+              </blockquote>
               <div className="relative z-20 pt-10 flex gap-3 flex-row mt-auto items-center">
                 <Avatar src="https://avatars.githubusercontent.com/u/4723117?v=4" />
-                <span className="flex flex-col">
-                  <span className=" leading-[1.6] text-slate-300 font-normal">
-                    {item.name}
-                  </span>
-                  <span className=" text-sm leading-[1.6] font-normal text-slate-300">
-                    {item.title}
-                  </span>
-                </span>
+                <div className="flex flex-col text-slate-300">
+                  <h3>{item.name}</h3>
+                  <p className=" text-sm">{item.title}</p>
+                </div>
               </div>
-            </blockquote>
+            </div>
           </li>
         ))}
       </ul>
