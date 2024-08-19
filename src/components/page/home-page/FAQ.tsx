66c2cfb379f0dcc82e826dc740ef4f1d477ef6fe
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-19 20:29:52 +0300

    refactor: Update FAQ component to use dynamic data from external file

--- src/components/page/home-page/FAQ.tsx
+++ src/components/page/home-page/FAQ.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 import SectionHeader from "@/components/ui/SectionHeader";
+import { FAQ_DATA } from "@/lib/data";
 
 const FAQ = () => {
   return (
@@ -14,44 +15,11 @@ const FAQ = () => {
           title: "text-emerald-900",
         }}
       >
-        <AccordionItem
-          key="1"
-          aria-label="How does AgriGuide work?"
-          title="How does AgriGuide work?"
-        >
-          AgriGuide uses AI image recognition to help African farmers identify
-          and manage crop pests and diseases accurately and efficiently. Farmers
-          upload pictures of their crops showing potential issues, and the AI
-          analyzes the images to identify the specific pest or disease,
-          providing targeted treatment recommendations.
-        </AccordionItem>
-        <AccordionItem
-          key="2"
-          aria-label="Who is this app targetting?"
-          title="Who is this app targetting?"
-        >
-          The app is designed for African farmers, including smallholder
-          farmers, agricultural extension services, NGOs, and larger
-          agribusinesses.
-        </AccordionItem>
-        <AccordionItem
-          key="3"
-          aria-label="What kind of pests and diseases can the app identify?"
-          title="What kind of pests and diseases can the app identify?"
-        >
-          The app uses a trained AI model which has a comprehensive database
-          covering a wide range of pests and diseases affecting various crops in
-          Africa.
-        </AccordionItem>
-        <AccordionItem
-          key="4"
-          aria-label="Are the treatment recommendations organic?"
-          title="Are the treatment recommendations organic?"
-        >
-          Yes, the app emphasizes sustainable practices by recommending organic
-          control methods whenever possible and providing targeted pesticide use
-          only when necessary.
-        </AccordionItem>
+        {FAQ_DATA.map((faq) => (
+          <AccordionItem key={faq.key} aria-label={faq.title} title={faq.title}>
+            {faq.content}
+          </AccordionItem>
+        ))}
       </Accordion>
     </section>
   );
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -58,3 +58,36 @@ export const PARTNERSHIPS = [
   },
   { src: "/assets/images/google.svg", alt: "Google logo", name: "Google" },
 ];
+
+export const FAQ_DATA = [
+  {
+    key: "1",
+    title: "How does AgriGuide work?",
+    content: `AgriGuide uses AI image recognition to help African farmers identify
+              and manage crop pests and diseases accurately and efficiently. Farmers
+              upload pictures of their crops showing potential issues, and the AI
+              analyzes the images to identify the specific pest or disease,
+              providing targeted treatment recommendations.`,
+  },
+  {
+    key: "2",
+    title: "Who is this app targeting?",
+    content: `The app is designed for African farmers, including smallholder
+              farmers, agricultural extension services, NGOs, and larger
+              agribusinesses.`,
+  },
+  {
+    key: "3",
+    title: "What kind of pests and diseases can the app identify?",
+    content: `The app uses a trained AI model which has a comprehensive database
+              covering a wide range of pests and diseases affecting various crops in
+              Africa.`,
+  },
+  {
+    key: "4",
+    title: "Are the treatment recommendations organic?",
+    content: `Yes, the app emphasizes sustainable practices by recommending organic
+              control methods whenever possible and providing targeted pesticide use
+              only when necessary.`,
+  },
+];
