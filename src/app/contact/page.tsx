5c791af9053ba67567b4af07a524853bd25ece4d
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-22 01:24:39 +0300

    refactor: Update ContactPage component to use SectionHeader as h1

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,16 @@
-import ContactForm from '@/components/ui/ContactForm';
-import SectionHeader from '@/components/ui/SectionHeader';
-import React from 'react'
+import ContactForm from "@/components/ui/ContactForm";
+import SectionHeader from "@/components/ui/SectionHeader";
+import React from "react";
 
 const ContactPage = () => {
   return (
-    <section className="h-[93vh] pt-24">
-      <SectionHeader className='my-0'>Contact Us</SectionHeader>
+    <div className="h-[93vh] pt-24">
+      <SectionHeader as="h1" className="m-0">
+        Contact Us
+      </SectionHeader>
       <ContactForm />
-    </section>
+    </div>
   );
-}
+};
 
-export default ContactPage
+export default ContactPage;
