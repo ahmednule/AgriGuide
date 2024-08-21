1016c9cdf5cd0b9a68ee91e8097cfd9523504b17
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-22 02:12:45 +0300

    refactor: Update ResourceNav component to ResourceMobileNav

--- src/app/resources/pests/page.tsx
+++ src/app/resources/pests/page.tsx
@@ -1,4 +1,4 @@
-import ResourceNav from "@/components/page/resource-page/ResourceNav";
+import ResourceMobileNav from "@/components/page/resource-page/ResourceMobileNav";
 import { ResourceType } from "@/lib/constants";
 import prisma from "@/lib/prisma";
 import Link from "next/link";
@@ -12,17 +12,17 @@ const AllPestsPage = async () => {
   });
   return (
     <>
-    <ResourceNav type={ResourceType.PESTS}/>
+      <ResourceMobileNav type={ResourceType.PESTS} />
       <h1 className="text-3xl font-bold mb-6">Plant Pests</h1>
       <p>
-        You will find helpful information about different pests that can
-        affect your crops. Each pest entry includes a clear description of what
-        the pest looks like, what crops it targets, and how it behaves. Images
-        will also be provided to help you recognize the pest easily. For each
-        pest, we provide practical tips on how to manage and control them,
-        including both natural and chemical methods. This resource is designed
-        to help you keep your crops healthy and thriving by identifying and
-        addressing pest problems early.
+        You will find helpful information about different pests that can affect
+        your crops. Each pest entry includes a clear description of what the
+        pest looks like, what crops it targets, and how it behaves. Images will
+        also be provided to help you recognize the pest easily. For each pest,
+        we provide practical tips on how to manage and control them, including
+        both natural and chemical methods. This resource is designed to help you
+        keep your crops healthy and thriving by identifying and addressing pest
+        problems early.
       </p>
       {
         <ul className="grid grid-cols-2 mt-8 text-emerald-600 md:grid-cols-3">
