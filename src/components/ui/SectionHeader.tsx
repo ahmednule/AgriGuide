24aa6c7f4e6eb6d1e3e0e94768f711c20e2917f3
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-22 01:24:46 +0300

    refactor: Update SectionHeader component to accept custom element type

--- src/components/ui/SectionHeader.tsx
+++ src/components/ui/SectionHeader.tsx
@@ -1,11 +1,25 @@
 import { cn } from "@nextui-org/react";
-import React, { PropsWithChildren } from "react";
+import React, { ElementType, ReactNode } from "react";
 
-const SectionHeader = ({ children, className }: {
-  children: React.ReactNode;
+const SectionHeader = ({
+  children,
+  className,
+  as: Component = "h2",
+}: {
+  children: ReactNode;
   className?: string;
+  as?: ElementType;
 }) => {
-  return <h2 className={cn(" text-center font-bold text-3xl text-emerald-950 my-10", className)}>{children}</h2>;
+  return (
+    <Component
+      className={cn(
+        " text-center font-bold text-3xl text-emerald-950 my-10",
+        className
+      )}
+    >
+      {children}
+    </Component>
+  );
 };
 
 export default SectionHeader;
