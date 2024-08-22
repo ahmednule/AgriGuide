8ff91add653045bb9c7570d3c399424f09755642
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-22 04:21:51 +0300

    refactor: Update layout.tsx to use notFound() instead of redirect() when user is not a customer

--- src/app/customer/layout.tsx
+++ src/app/customer/layout.tsx
@@ -1,12 +1,12 @@
 import { auth } from "@/auth";
 import CustomerAside from "@/components/page/customer-page/CustomerAside";
 import { Role } from "@prisma/client";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import React, { PropsWithChildren } from "react";
 
 const layout = async ({ children }: PropsWithChildren) => {
   const session = await auth();
-  if (session?.user.role !== Role.CUSTOMER) redirect("/");
+  if (session?.user.role !== Role.CUSTOMER) notFound();
   return (
     <div className="h-[93vh] flex bg-emerald-50">
       <CustomerAside />
