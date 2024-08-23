8399d4d955cd0c47e880791dbd2c0c577dafebb6
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-23 09:14:42 +0300

    refactor: Update AdminAside and CustomerAside components to adjust padding and add responsive styling

--- src/components/page/admin-page/AdminAside.tsx
+++ src/components/page/admin-page/AdminAside.tsx
@@ -3,7 +3,7 @@ import AdminNav from "./AdminNav";
 
 const AdminAside = async () => {
   return (
-    <aside className=" w-1/6 bg-gradient-to-r overflow-y-auto p-10 pt-20 hidden lg:block from-emerald-800 to-emerald-700 h-full">
+    <aside className=" w-1/6 bg-gradient-to-r overflow-y-auto p-5 xl:px-10 pt-20 hidden lg:block from-emerald-800 to-emerald-700 h-full">
       <AdminNav />
     </aside>
   );
--- src/components/page/customer-page/CustomerAside.tsx
+++ src/components/page/customer-page/CustomerAside.tsx
@@ -3,7 +3,7 @@ import CustomerNav from "./CustomerNav";
 
 const CustomerAside = async () => {
   return (
-    <aside className=" w-1/6 bg-gradient-to-r overflow-y-auto p-10 pt-20 hidden lg:block from-emerald-800 to-emerald-700 h-full">
+    <aside className=" w-1/6 bg-gradient-to-r overflow-y-auto p-5 xl:px-10 pt-20 hidden lg:block from-emerald-800 to-emerald-700 h-full">
       <CustomerNav />
     </aside>
   );
