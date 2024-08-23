b71b5fe99844b47e7a370898562da09588a46f7c
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-23 08:00:27 +0300

    refactor: Update CustomerPage component to include MobileNav and buttons for scan history, progress tracker, and consultant chat

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -1,12 +1,42 @@
-import { auth } from "@/auth";
-import { Role } from "@prisma/client";
-import { notFound, redirect } from "next/navigation";
+import MobileNav from "@/components/ui/MobileNav";
+import { Button, Link } from "@nextui-org/react";
 
-const CustomerPage = async () => {
-  const session = await auth();
-  const user = session?.user;
-  if (!user || user.role !== Role.CUSTOMER) notFound();
-  redirect("/customer/scan-history");
+const CustomerPage = () => {
+  return (
+    <>
+      <MobileNav />
+      <h1 className="text-3xl text-emerald-800 font-bold">Customer Panel</h1>
+      <p className="text-emerald-800 mt-5">
+        This panel allows you to easily access your Scan History to review past
+        diagnoses, track the status of ongoing scans with the Progress Tracker,
+        and connect with experts via the Consultant Chat for personalized
+        support.
+      </p>
+      <div className="gap-3 mt-5 flex flex-wrap">
+        <Button
+          as={Link}
+          href="/customer/scan-history"
+          className="!bg-emerald-600 text-white"
+        >
+          Scan history
+        </Button>
+        <Button
+          as={Link}
+          href="/customer/progress-tracker"
+          className="!bg-emerald-600 text-white"
+        >
+          Progress tracker
+        </Button>
+        <Button
+          as={Link}
+          href="/customer/contact-consultant"
+          className="!bg-emerald-600 text-white"
+        >
+          Consultant chat
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default CustomerPage;
