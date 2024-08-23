848b12d79c95641ffe81d16d8ffce424cbde7b31
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-23 09:05:44 +0300

    feat: Change layout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,49 @@
-import { auth } from "@/auth";
-import { Role } from "@prisma/client";
-import { notFound, redirect } from "next/navigation";
+import MobileNav from "@/components/ui/MobileNav";
+import { Button, Link } from "@nextui-org/react";
 
-const AdminPage = async () => {
-  const session = await auth();
-  const user = session?.user;
-  if (!user || user.role !== Role.ADMIN) notFound();
-  redirect("/admin/dashboard");
+const AdminPage = () => {
+  return (
+    <>
+      <MobileNav />
+      <h1 className="text-3xl text-emerald-800 font-bold">Admin Panel</h1>
+      <p className="text-emerald-800 mt-5">
+        This interface provides streamlined access to essential administrative
+        functions, including the dashboard, customer management, consultant
+        oversight, and fellow admin coordination. Utilize the tools below to
+        efficiently manage and oversee operations.
+      </p>
+      <div className="gap-3 mt-5 flex flex-wrap">
+        <Button
+          as={Link}
+          href="/admin/dashboard"
+          className="!bg-emerald-600 text-white"
+        >
+          Dashboard
+        </Button>
+        <Button
+          as={Link}
+          href="/admin/customers"
+          className="!bg-emerald-600 text-white"
+        >
+          Customers
+        </Button>
+        <Button
+          as={Link}
+          href="/admin/consultants"
+          className="!bg-emerald-600 text-white"
+        >
+          Consultants
+        </Button>
+        <Button
+          as={Link}
+          href="/admin/admins"
+          className="!bg-emerald-600 text-white"
+        >
+          Admin
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default AdminPage;
