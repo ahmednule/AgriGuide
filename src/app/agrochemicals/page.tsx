0123ae3a09aef99790228312671ca0359cbafc4e
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-25 09:32:29 +0530

    refactor: Update AgrochemicalsPage component to fetch products with suppliers

--- src/app/agrochemicals/page.tsx
+++ src/app/agrochemicals/page.tsx
@@ -1,13 +1,32 @@
 import AgrochemicalProducts from "@/components/agrochemicals-page/AgrochemicalProducts";
 import SectionHeader from "@/components/ui/SectionHeader";
+import prisma from "@/lib/prisma";
 
-const AgrochemicalsPage = () => {
+const AgrochemicalsPage = async () => {
+  const productsWithSupplier = await prisma.productSupplier.findMany({
+    select: {
+      price: true,
+      location: true,
+      images: true,
+      product: {
+        select: {
+          name: true,
+          id: true,
+        },
+      },
+      supplier: {
+        select: {
+          name: true,
+        },
+      },
+    }
+  });
   return (
     <main className="min-h-[93vh] pt-20">
       <SectionHeader as="h1" className="mt-0">
         Agrochemical Products
       </SectionHeader>
-      <AgrochemicalProducts />
+      <AgrochemicalProducts productsWithSupplier={productsWithSupplier} />
     </main>
   );
 };
