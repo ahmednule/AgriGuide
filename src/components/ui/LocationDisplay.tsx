391b19dcd20cf27e7d7175a14e5bdf2ff7e05994
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-25 14:11:28 +0530

    refactor: Add LocationDisplay component for displaying geolocation information

---
+++ src/components/ui/LocationDisplay.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import useGeolocation from "@/lib/hooks/useGeolocation";
+import { Skeleton, Spinner } from "@nextui-org/react";
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
+
+const LocationDisplay = () => {
+  const { location, error, isLoading } = useGeolocation();
+  const country = location.country;
+  const city = location.city;
+
+  useEffect(() => {
+    if (error) toast.error(error);
+  }, [error]);
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center mt-1 mb-8">
+        <Skeleton isLoaded={!isLoading} className=" h-6 w-36 rounded bg-emerald-100" />
+      </div>
+    );
+  else
+    return (
+      <div className="text-center mt-1 mb-8 text-emerald-600">
+        {city}, {country}
+      </div>
+    );
+};
+
+export default LocationDisplay;
