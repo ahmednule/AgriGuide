d0bdaa4968f5e89a02d2cc70d2afa1327f3166a8
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-28 11:50:44 +0530

    feat: Add SuppliersPage component and update AdminDashboard to display a list of suppliers

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -3,6 +3,7 @@ import MobileNav from "@/components/ui/MobileNav";
 import prisma from "@/lib/prisma";
 import {
   faArrowRight,
+  faBoxesStacked,
   faUser,
   faUserDoctor,
   faUserTie,
@@ -18,6 +19,7 @@ const AdminDashboard = async () => {
   const customers = users.filter((user) => user.role === Role.CUSTOMER);
   const consultants = users.filter((user) => user.role === Role.CONSULTANT);
   const admins = users.filter((user) => user.role === Role.ADMIN);
+  const suppliers = users.filter((user) => user.role === Role.SUPPLIER);
   return (
     <>
       <MobileNav />
@@ -82,6 +84,34 @@ const AdminDashboard = async () => {
           </Card>
           <Card className="bg-emerald-100 w-full">
             <CardHeader className="text-xl font-bold text-emerald-900">
+              <FontAwesomeIcon icon={faBoxesStacked} className="mr-4 text-2xl" />
+              <h2>Suppliers</h2>
+            </CardHeader>
+            <CardBody className="text-emerald-800 text-center p-10 space-y-10">
+              <div className="flex justify-between items-center">
+                <p>
+                  <span className="font-bold text-xl rounded-full p-3 mr-2 bg-emerald-300">
+                    {suppliers.length}
+                  </span>
+                  Total Suppliers
+                </p>
+                <Button
+                  as={Link}
+                  href="/admin/suppliers"
+                  className="bg-emerald-700 text-white"
+                  isIconOnly
+                >
+                  <FontAwesomeIcon
+                    title="View more"
+                    icon={faArrowRight}
+                    size="xl"
+                  />
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+          <Card className="bg-emerald-100 w-full">
+            <CardHeader className="text-xl font-bold text-emerald-900">
               <FontAwesomeIcon icon={faUserTie} className="mr-4 text-2xl" />
               <h2>Admins</h2>
             </CardHeader>
@@ -115,6 +145,7 @@ const AdminDashboard = async () => {
         <div className="space-y-4">
           <UserDataChart label="Customers Joined" users={customers} />
           <UserDataChart label="Consultants Joined" users={consultants} />
+          <UserDataChart label="Suppliers Joined" users={suppliers} />
           <UserDataChart label="Admins Joined" users={admins} />
         </div>
       </section>
