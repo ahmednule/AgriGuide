4281f132424d0b3acbacfdcc5f630fd27d83ff80
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-28 11:50:35 +0530

    feat: Update CustomersPage to display customer details with scans

--- src/app/admin/customers/page.tsx
+++ src/app/admin/customers/page.tsx
@@ -1,20 +1,36 @@
+import CustomerTable from "@/components/page/admin-page/CustomerTable";
 import UserTable from "@/components/page/admin-page/UserTable";
 import MobileNav from "@/components/ui/MobileNav";
 import prisma from "@/lib/prisma";
-import { Role } from "@prisma/client";
+import { CustomerScan } from "@/lib/types";
+import { Customer, Role, Scan, User } from "@prisma/client";
 import React from "react";
 
 const CustomersPage = async () => {
-  const customers = await prisma.user.findMany({
-    where: {
-      role: Role.CUSTOMER,
-    },
-  });
+const customers = await prisma.customer.findMany({
+  include: {
+    scan: true,
+    user: true,
+  },
+}).then(customers =>
+  customers.map(customer => ({
+    id: customer.id,
+    role: customer.user.role,
+    image: customer.user.image,
+    name: customer.user.name,
+    totalScans: customer.scan.length,
+    updatedAt: customer.user.updatedAt,
+    createdAt: customer.user.createdAt,
+    userName: customer.user?.name,
+    userEmail: customer.user?.email,
+  }))
+) as CustomerScan[];
+
   return (
     <>
       <MobileNav />
       <h1 className="text-3xl font-bold text-emerald-900 mb-8">Customers</h1>
-      <UserTable users={customers} />
+      <CustomerTable customer={customers}/>
     </>
   );
 };
