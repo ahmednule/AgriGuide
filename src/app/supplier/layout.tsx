7426f07cf9ecf2094332af5afea8f87b66f2792f
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-30 12:20:56 +0530

    feat: Add redirection for supplier status

--- src/app/supplier/layout.tsx
+++ src/app/supplier/layout.tsx
@@ -1,13 +1,24 @@
 import { auth } from "@/auth";
 import SupplierNav from "@/components/page/supplier-page/SupplierNav";
 import Aside from "@/components/ui/Aside";
-import { Role } from "@prisma/client";
-import { notFound } from "next/navigation";
+import prisma from "@/lib/prisma";
+import { Role, SupplierStatus } from "@prisma/client";
+import { notFound, redirect } from "next/navigation";
 import React, { PropsWithChildren } from "react";
 
 const layout = async ({ children }: PropsWithChildren) => {
   const session = await auth();
   if (session?.user.role !== Role.SUPPLIER) notFound();
+  const supplier = await prisma.supplier.findUnique({
+    where: { id: session.user.id },
+  });
+
+  if (supplier?.status === SupplierStatus.PENDING) {
+    redirect("/#pending");
+  } else if (supplier?.status === SupplierStatus.REJECTED) {
+    redirect("/#rejected");
+  }
+
   return (
     <div className="h-[93vh] flex bg-emerald-50">
       <Aside>
