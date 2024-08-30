6b362de2d9ce048f7260f5d595559e8f4bfd9c4c
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-08-30 18:45:54 +0530

    refactor: Update AdminNav to use isLinkActive utility function

--- src/components/page/admin-page/AdminNav.tsx
+++ src/components/page/admin-page/AdminNav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ADMIN_ROUTES } from "@/lib/constants";
+import { isLinkActive } from "@/lib/utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, cn, Divider } from "@nextui-org/react";
 import Link from "next/link";
@@ -9,7 +10,6 @@ import React from "react";
 
 const AdminNav = () => {
   const pathname = usePathname();
-  const isRoute = (route: string) => route === pathname;
   return (
     <nav>
       <ul className="space-y-4">
@@ -24,7 +24,10 @@ const AdminNav = () => {
                 className={cn(
                   " w-full gap-3 !justify-start text-emerald-300 hover:!bg-emerald-200/85 hover:text-emerald-700",
                   {
-                    "bg-emerald-200 text-emerald-700": isRoute(route.path),
+                    "bg-emerald-200 text-emerald-700": isLinkActive({
+                      pathname,
+                      route: route.path,
+                    }),
                   }
                 )}
                 variant="light"
