ea64ab6cb99c03906eda0fc4ad8f9e5973b12543
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-09-02 12:51:49 +0530

    refactor: Update ImageUpload component to support passing initial files

--- src/components/ui/ImageUpload.tsx
+++ src/components/ui/ImageUpload.tsx
@@ -1,5 +1,10 @@
-// ImageUpload.tsx
-import React, { useRef, forwardRef, useImperativeHandle, ForwardedRef } from "react";
+import React, {
+  useRef,
+  forwardRef,
+  useImperativeHandle,
+  useState,
+  useEffect,
+} from "react";
 import { FilePond, registerPlugin } from "react-filepond";
 import "filepond/dist/filepond.min.css";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
@@ -17,25 +22,58 @@ registerPlugin(
   FilePondPluginFileValidateSize
 );
 
-const ImageUpload = forwardRef(({ name, allowMultiple = false, files}: { files?: string[], name: string, allowMultiple?: boolean }, ref: ForwardedRef<FilePond>) => {
-  return (
-    <div className="cursor-pointer">
-      <FilePond
-        ref={ref}
-        required
-        files = {files}
-        allowReorder={allowMultiple}
-        acceptedFileTypes={["image/*"]}
-        name={name}
-        labelFileTypeNotAllowed="Invalid file type. Please upload an image"
-        allowMultiple={allowMultiple}
-        labelFileProcessingError="An error occurred during processing"
-        labelIdle="Drag & Drop or Browse your desired image"
-        maxFileSize="1MB"
-      />
-    </div>
-  );
-});
+const ImageUpload = forwardRef(
+  (
+    {
+      name,
+      allowMultiple = false,
+      files: initialFiles = [],
+    }: { files?: string[]; name: string; allowMultiple?: boolean },
+    ref
+  ) => {
+    const pondRef = useRef<FilePond>(null);
+    const [files, setFiles] = useState<any>(initialFiles);
+    const [isInitialized, setIsInitialized] = useState(false);
+
+    useImperativeHandle(ref, () => ({
+      reset: () => {
+        if (pondRef.current) {
+          pondRef.current.removeFiles();
+        }
+        setFiles([]);
+      },
+    }));
+
+    useEffect(() => {
+      if (!isInitialized) {
+        setFiles(initialFiles);
+        setIsInitialized(true);
+      }
+    }, [initialFiles, isInitialized]);
+
+    return (
+      <div className="cursor-pointer">
+        <FilePond
+          ref={pondRef}
+          required
+          files={files}
+          // onupdatefiles={(fileItems) => {
+          //   setFiles(fileItems.map((fileItem) => fileItem.file));
+          // }}
+          onupdatefiles={setFiles}
+          allowReorder={allowMultiple}
+          acceptedFileTypes={["image/*"]}
+          name={name}
+          labelFileTypeNotAllowed="Invalid file type. Please upload an image"
+          allowMultiple={allowMultiple}
+          labelFileProcessingError="An error occurred during processing"
+          labelIdle="Drag & Drop or Browse your desired image"
+          maxFileSize="1MB"
+        />
+      </div>
+    );
+  }
+);
 
 // Add a display name to the component
 ImageUpload.displayName = "ImageUpload";
