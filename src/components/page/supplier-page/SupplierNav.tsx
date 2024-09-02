9588b427cba4afa03efeecf6b6fa7ea51057709b
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-09-02 13:00:18 +0530

    refactor: Update SupplierNav component to use isLinkActive utility function for determining active route

--- src/components/page/supplier-page/SupplierNav.tsx
+++ src/components/page/supplier-page/SupplierNav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SUPPLIER_ROUTES } from "@/lib/constants";
+import { isLinkActive } from "@/lib/utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, cn, Divider } from "@nextui-org/react";
 import Link from "next/link";
@@ -9,7 +10,6 @@ import React from "react";
 
 const SupplierNav = () => {
   const pathname = usePathname();
-  const isRoute = (route: string) => route === pathname;
   return (
     <nav>
       <ul className="space-y-4">
@@ -24,7 +24,10 @@ const SupplierNav = () => {
                 className={cn(
                   " w-full gap-3 !justify-start text-emerald-300 hover:!bg-emerald-200/85 hover:text-emerald-700",
                   {
-                    "bg-emerald-200 text-emerald-700": isRoute(route.path),
+                    "bg-emerald-200 text-emerald-700": isLinkActive({
+                      pathname,
+                      route: route.path,
+                    }),
                   }
                 )}
                 variant="light"
