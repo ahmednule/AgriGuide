c63b214d8b60ce192c531fd35473bf51844f17b3
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-09-02 12:58:09 +0530

    refactor: Update AgrochemicalsFilter component to include filters for name, supplier, price, and location

--- src/components/page/store-page/AgrochemicalsFilter.tsx
+++ src/components/page/store-page/AgrochemicalsFilter.tsx
@@ -1,83 +1,198 @@
+import { initialPriceRange } from "@/lib/constants";
 import useGeolocation from "@/lib/hooks/useGeolocation";
-import { ProductWithSuppliers } from "@/lib/types";
-import { Button, Select, SelectItem } from "@nextui-org/react";
-import React from "react";
+import { ProductWithSuppliers, TLocation } from "@/lib/types";
+import {
+  Autocomplete,
+  AutocompleteItem,
+  Button,
+  Image,
+  Select,
+  SelectItem,
+  Slider,
+  Spinner,
+} from "@nextui-org/react";
+import React, { useEffect } from "react";
 
 const AgrochemicalsFilter = ({
-  filterName,
-  setFilterName,
-  filterSupplier,
-  setFilterSupplier,
-  filterPrice,
-  setFilterPrice,
+  nameFilter,
+  setNameFilter,
+  supplierFilter,
+  setSupplierFilter,
+  locationFilter,
+  setLocationFilter,
+  priceRange,
+  setPriceRange,
+  uniqueProductLocations,
   productsWithSuppliers,
 }: {
-  filterName: Set<string>;
-  setFilterName: any;
-  filterSupplier: Set<string>;
-  setFilterSupplier: any;
-  filterPrice: Set<string>;
-  setFilterPrice: any;
+  nameFilter: string;
+  locationFilter: string;
+  setLocationFilter: React.Dispatch<React.SetStateAction<string>>;
+  setNameFilter: any;
+  supplierFilter: string;
+  setSupplierFilter: any;
+  priceRange: number | number[];
+  uniqueProductLocations: TLocation[];
+  setPriceRange: any;
   productsWithSuppliers: Omit<ProductWithSuppliers, "description">[];
 }) => {
-  const { location: { country_name: country, city } = {}, isLoading } =
-    useGeolocation();
+  const {
+    location: { country_name: country, city, state_prov: region } = {},
+    isLoading,
+  } = useGeolocation();
+
+  useEffect(() => {
+    if (country && city && region) {
+      setLocationFilter(`${city}, ${region}, ${country}`);
+    }
+  }, [country, city, region]);
 
   const filteredProducts = productsWithSuppliers.filter(
     (product) => product.city === city && product.country === country
   );
 
   const names = Array.from(
-    new Set(filteredProducts.map((product) => product.product.name))
+    new Set(
+      filteredProducts.map((product) => ({
+        name: product.product.name,
+        id: product.product.id,
+      }))
+    )
   );
+
   const suppliers = Array.from(
-    new Set(filteredProducts.map((product) => product.supplier.name))
-  );
-  const prices = Array.from(
-    new Set(filteredProducts.map((product) => product.price.toString()))
+    new Set(
+      filteredProducts.map((product) => ({
+        name: product.supplier.name,
+        id: product.supplier.id,
+      }))
+    )
   );
 
   const handleClear = () => {
-    setFilterName(new Set([]));
-    setFilterSupplier(new Set([]));
-    setFilterPrice(new Set([]));
+    setNameFilter("");
+    setSupplierFilter("");
+    setLocationFilter("");
+    setPriceRange(initialPriceRange);
   };
 
   return (
-    <div className="px-9 mb-8 flex sticky top-0 w-full justify-between items-center gap-3 z-10">
-      <Select
-        onSelectionChange={setFilterName}
-        selectedKeys={filterName}
-        label="Filter name"
+    <div className="px-9 mb-8 flex sticky top-0 w-full justify-between items-center gap-5 z-10">
+      <Autocomplete
+        allowsCustomValue
         color="success"
         className="max-w-xs"
+        name="name"
+        inputValue={nameFilter}
+        onInputChange={setNameFilter}
+        defaultItems={names}
+        label="Filter product name"
       >
-        {names.map((name) => (
-          <SelectItem key={name}>{name}</SelectItem>
-        ))}
-      </Select>
-      <Select
-        onSelectionChange={setFilterSupplier}
-        selectedKeys={filterSupplier}
-        label="Filter supplier"
+        {({ id, name }) => <AutocompleteItem key={id}>{name}</AutocompleteItem>}
+      </Autocomplete>
+      {/* <Autocomplete
+        allowsCustomValue
         color="success"
         className="max-w-xs"
+        name="name"
+        inputValue={typeFilter}
+        onInputChange={setTypeFilter}
+        defaultItems={names}
+        label="Filter product name"
       >
-        {suppliers.map((supplier) => (
-          <SelectItem key={supplier}>{supplier}</SelectItem>
-        ))}
-      </Select>
-      <Select
-        onSelectionChange={setFilterPrice}
-        selectedKeys={filterPrice}
-        label="Filter price"
+        {({ id, name }) => <AutocompleteItem key={id}>{name}</AutocompleteItem>}
+      </Autocomplete> */}
+      <Autocomplete
+        allowsCustomValue
         color="success"
         className="max-w-xs"
+        name="name"
+        inputValue={supplierFilter}
+        onInputChange={setSupplierFilter}
+        defaultItems={suppliers}
+        label="Filter supplier name"
       >
-        {prices.sort().map((price) => (
-          <SelectItem key={price}>{price}</SelectItem>
-        ))}
-      </Select>
+        {({ id, name }) => <AutocompleteItem key={id}>{name}</AutocompleteItem>}
+      </Autocomplete>
+      <Slider
+        label="Filter price range"
+        className="max-w-xs"
+        color="success"
+        step={10}
+        minValue={initialPriceRange[0]}
+        maxValue={initialPriceRange[1]}
+        value={priceRange}
+        onChange={setPriceRange}
+      />
+      {isLoading && (
+        <Autocomplete
+          color="success"
+          className="max-w-xs"
+          name="location"
+          isLoading={true}
+          isDisabled={true}
+          label="Filter location"
+        >
+          <AutocompleteItem key="" />
+        </Autocomplete>
+      )}
+      {!locationFilter && !isLoading && (
+        <Autocomplete
+          allowsCustomValue
+          color="success"
+          className="max-w-xs"
+          name="location"
+          isLoading={isLoading}
+          isDisabled={isLoading}
+          onInputChange={setLocationFilter}
+          defaultItems={uniqueProductLocations}
+          defaultSelectedKey={locationFilter}
+          label="Filter location"
+        >
+          {({ city, country, region, countryCode }) => (
+            <AutocompleteItem
+              startContent={
+                <Image
+                  src={`https://flagcdn.com/48x36/ke.png`}
+                  alt={`${city}, ${region}, ${country}`}
+                  className="w-5 h-5 rounded-full mr-2"
+                />
+              }
+              key={`${city}, ${region}, ${country}`}
+            >
+              {`${city}, ${region}, ${country}`}
+            </AutocompleteItem>
+          )}
+        </Autocomplete>
+      )}
+      {!isLoading && locationFilter && (
+        <Autocomplete
+          allowsCustomValue
+          color="success"
+          className="max-w-xs"
+          name="location"
+          inputValue={locationFilter}
+          onInputChange={setLocationFilter}
+          defaultItems={uniqueProductLocations}
+          defaultSelectedKey={locationFilter}
+          label="Filter location"
+        >
+          {({ city, country, region, countryCode }) => (
+            <AutocompleteItem
+              startContent={
+                <Image
+                  src={`https://flagcdn.com/48x36/ke.png`}
+                  alt={`${city}, ${region}, ${country}`}
+                  className="w-5 h-5 rounded-full mr-2"
+                />
+              }
+              key={`${city}, ${region}, ${country}`}
+            >
+              {`${city}, ${region}, ${country}`}
+            </AutocompleteItem>
+          )}
+        </Autocomplete>
+      )}
 
       <Button color="danger" onPress={handleClear}>
         Clear
