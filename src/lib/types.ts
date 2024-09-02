6e71b54a2319881f1f941340338469c9433c1712
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-09-02 14:14:49 +0530

    refactor: Update GeolocationData type to include additional location information

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -152,3 +152,37 @@ export type TLocation = {
   country: string;
   region: string;
 };
+
+ type LocationInfo = {
+  name: string;
+  description?: string;
+  isoName?: string;
+  order: number;
+  adminLevel?: number;
+  isoCode?: string;
+  wikidataId?: string;
+  geonameId?: number;
+};
+
+type LocalityInfo = {
+  administrative: LocationInfo[];
+  informative: LocationInfo[];
+};
+
+export type GeolocationData = {
+  latitude: number;
+  lookupSource: string;
+  longitude: number;
+  localityLanguageRequested: string;
+  continent: string;
+  continentCode: string;
+  countryName: string;
+  countryCode: string;
+  principalSubdivision: string;
+  principalSubdivisionCode: string;
+  city: string;
+  locality: string;
+  postcode: string;
+  plusCode: string;
+  localityInfo: LocalityInfo;
+};
